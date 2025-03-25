703aa6f2b7cef53bbdab8cfdc7f76789facde7d1
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-03-25 21:49:36 +0100

    download_ticker: introduced _test flag

--- src/download_ticker.py
+++ src/download_ticker.py
@@ -10,17 +10,32 @@ class DownloadTicker():
               self,
               ticker,
               period, # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+              test=False,
               log_level='info' # valid levels: debug, info
               ):
+        self.__config_logger(log_level)
+        self.logger = logging.getLogger(__name__)
+        if test:
+            self.logger.info(
+                'Initialized DownloadTicker in test mode')
+        else:
+            self.logger.info(
+                'Initialized DownloadTicker in production mode')
+
+        # input arguments
         self.ticker = ticker
         self.period = period
-        self.rootpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.test = test
+
+        # set datapath and create data directory
+        self.rootpath = os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__)))
         self.datapath = os.path.join(self.rootpath, 'data')
         if not os.path.exists(self.datapath):
             os.makedirs(self.datapath)
-
-        self.__config_logger(log_level)
-        self.logger = logging.getLogger(__name__)
+            self.logger.info(
+                'Created data directory {}'.format(self.datapath))
 
     def __config_logger(self, log_level):
         log_format = '%(asctime)s - %(levelname)s - %(message)s'
@@ -36,6 +51,8 @@ class DownloadTicker():
             raise Exception ('log_level must be "debug" or "info"')
 
     def download_silent(self):
+        self.logger.info(
+            'Downloading data for {}'.format(self.ticker))
         # silence the verbose YF-API
         with open(os.devnull, 'w') as devnull:
             with contextlib.redirect_stdout(devnull):
@@ -44,14 +61,14 @@ class DownloadTicker():
                     period=self.period)
                 if len(data.index) == 0:
                     self.logger.warning(
-                        'No data downloaded for {}'.format(self.ticker))
+                        'No data downloaded')
                 else:
+                    suffix = '_test' if self.test else ''
                     fpath = os.path.join(
-                            self.datapath,'{}.csv'.format(self.ticker)
-                            )
+                            self.datapath,'{}{}.csv'.format(self.ticker, suffix))
                     data.to_csv(fpath)
                     self.logger.info(
-                        'Data downloaded for {}'.format(self.ticker))
+                        'Data downloaded to {}'.format(fpath))
 
 
 if __name__ == '__main__':
