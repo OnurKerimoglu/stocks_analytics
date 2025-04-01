1c96cc90f71c4fadd1079957d96d0d80540f16f5
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-04-01 10:27:39 +0200

    adapted test_download_ticker

--- tests/test_download_ticker.py
+++ tests/test_download_ticker.py
@@ -5,10 +5,11 @@ ticker = 'MSFT'
 download_ticker = DownloadTicker(
         ticker=ticker,
         period='1d',
+        out_format='parquet',
         test=True)
 
-def test_download_silent():
-    download_ticker.download_silent()
-    fpath = os.path.join(download_ticker.datapath, '{}_test.csv'.format(ticker))
+def test_download():
+    download_ticker.download()
+    fpath = os.path.join(download_ticker.datapath, '{}_test.{}'.format(ticker, download_ticker.out_format))
     assert os.path.exists(fpath)
     os.remove(fpath)
