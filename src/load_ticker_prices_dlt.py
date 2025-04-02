d9c44030d6fcaf3a87c2396d96d1367ba7e48825
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-04-02 11:31:28 +0200

    load_ticker_prices_dlt: introduced fundamentals_pipeline

--- src/load_ticker_prices_dlt.py
+++ src/load_ticker_prices_dlt.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import dlt
+import json
 import pandas as pd
 
 from shared import config_logger
@@ -30,42 +31,58 @@ class LoadTickerPrices():
         self.rootpath = os.path.dirname(
             os.path.dirname(
                 os.path.abspath(__file__)))
-        self.datapath = os.path.join(self.rootpath, 'data', 'price')
-        self.parquet_paths = self.fetch_parquet_paths()
+        self.datapath_price = os.path.join(self.rootpath, 'data', 'price')
+        self.datapath_fund = os.path.join(self.rootpath, 'data', 'info')
+        self.price_paths = self.fetch_data_paths(self.datapath_price, 'parquet')
+        self.fund_paths = self.fetch_data_paths(self.datapath_fund, 'json')
+
+        gcp_key_fpath = "/home/onur/gcp-keys/stocks-455113-eb2c3f563c78.json"
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_key_fpath
 
         # Define dlt pipelines
-        self.pipeline_duckdb = dlt.pipeline(
+        # Price
+        self.price_pipeline_duckdb = dlt.pipeline(
             pipeline_name='load_stock_prices_raw_duckb',
             destination='duckdb',
             dataset_name='stocks_raw',
             dev_mode=self.dev_mode
         )
-        
-        gcp_key_fpath = "/home/onur/gcp-keys/stocks-455113-eb2c3f563c78.json"
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_key_fpath
-        self.pipeline_bq = dlt.pipeline(
+        self.price_pipeline_bq = dlt.pipeline(
             pipeline_name="load_stock_prices_raw_bq",
             destination="bigquery",
             dataset_name="stocks_raw",
             dev_mode=self.dev_mode
         )
+        # Fundamentals
+        self.fundamentals_pipeline_duckdb = dlt.pipeline(
+            pipeline_name='load_stock_fundamentals_raw_duckb',
+            destination='duckdb',
+            dataset_name='stocks_raw',
+            dev_mode=self.dev_mode
+        )
+        self.fundamentals_pipeline_bq = dlt.pipeline(
+            pipeline_name="load_stock_fundamentals_raw_bq",
+            destination="bigquery",
+            dataset_name="stocks_raw",
+            dev_mode=self.dev_mode
+        )
     
-    def fetch_parquet_paths(self):
+    def fetch_data_paths(self, datapath, ext):
         # construct a list of absolute paths based on the 
         # (non-test) parquet files in the self.datapth folder
         fpaths = []
-        for f in os.listdir(self.datapath):
-            if f.endswith('.parquet') and not f.endswith('_test.parquet'):
-                fpaths.append(os.path.join(self.datapath, f))
+        for f in os.listdir(datapath):
+            if f.endswith(f'.{ext}') and not f.endswith(f'_test.{ext}'):
+                fpaths.append(os.path.join(datapath, f))
         if len(fpaths) == 0:
-            self.logger.warning(f'No parquet files found at {self.datapath}')
+            self.logger.warning(f'No {ext} files found at {datapath}')
         else:
-            self.logger.info(f'Found {len(fpaths)} parquet files at {self.datapath}')
+            self.logger.info(f'Found {len(fpaths)} {ext} files at {datapath}')
         return fpaths
 
     @dlt.resource(name="prices")
     @staticmethod
-    def stocks_raw(
+    def stock_prices_raw(
         parquet_paths,
         load_type,
         cursor_date=dlt.sources.incremental(
@@ -77,12 +94,23 @@ class LoadTickerPrices():
             df = pd.read_parquet(fpath)
             print(f'{load_type}-load from {fpath}')
             yield df.to_dict(orient="records")
+    
+    @dlt.resource(name="fundamentals", primary_key="symbol")
+    @staticmethod
+    def stock_fundamentals_raw(
+        json_paths
+        ):
+        for fpath in json_paths:
+            with open(fpath, encoding='utf-8') as f:
+                d = json.load(f)
+            print(f'Full-load from {fpath}')
+            yield d
 
-    def run_pipeline(self):
+    def run_price_pipeline(self):
         if self.dest == 'duckdb':
-            pipeline = self.pipeline_duckdb
+            pipeline = self.price_pipeline_duckdb
         elif self.dest == 'bigquery':
-            pipeline = self.pipeline_bq
+            pipeline = self.price_pipeline_bq
         else:
             raise ValueError(f"Unknown dest: {self.dest}. Accepted: 'duckdb', 'bigquery'")
         if self.full_load:
@@ -92,8 +120,25 @@ class LoadTickerPrices():
             write_disp = 'append'
             load_type = 'incremental'
         info = pipeline.run(
-            self.stocks_raw(self.parquet_paths, load_type),
+            self.stock_prices_raw(self.price_paths, load_type),
             table_name='stock_prices',
+            loader_file_format="jsonl",
+            write_disposition=write_disp)
+        # self.logger.info(info)
+        self.logger.info(pipeline.last_trace)
+
+
+    def run_fundamentals_pipeline(self):
+        if self.dest == 'duckdb':
+            pipeline = self.fundamentals_pipeline_duckdb
+        elif self.dest == 'bigquery':
+            pipeline = self.fundamentals_pipeline_bq
+        else:
+            raise ValueError(f"Unknown dest: {self.dest}. Accepted: 'duckdb', 'bigquery'")
+        write_disp = 'merge'
+        info = pipeline.run(
+            self.stock_fundamentals_raw(self.fund_paths),
+            table_name='stock_fundamentals',
             write_disposition=write_disp)
         # self.logger.info(info)
         self.logger.info(pipeline.last_trace)
@@ -106,5 +151,6 @@ if __name__ == '__main__':
         dest='bigquery',
         dev_mode=False,
         log_level='info')
-    load_ticker.run_pipeline()
+    # load_ticker.run_price_pipeline()
+    load_ticker.run_fundamentals_pipeline()
     
