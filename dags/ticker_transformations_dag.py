8887e4bbd7e263f0327edd83f0a0feb0336a6ae4
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-05-06 01:43:14 +0200

    ticker_transformations_dag: xcom_pull env from ingestion dag

--- dags/ticker_transformations_dag.py
+++ dags/ticker_transformations_dag.py
@@ -1,7 +1,8 @@
 import os
 import logging
+import subprocess
 
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 from airflow.models.param import Param
 from airflow.operators.bash import BashOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
@@ -27,18 +28,42 @@ dbt_dir = os.path.join(rootpath, 'dbt', 'stocks_dbt')
 )
 def ticker_transformations_dag():
 
-    price_technicals = BashOperator(
-        task_id='price_technicals_lastday',
-        bash_command=f"dbt run -s price_technicals_lastday  --profiles-dir {dbt_dir}/config --project-dir {dbt_dir}"
-    )
+    @task
+    def pull_env(**context):
+        # Pull the value from ingestion dag's task instance
+        task_ids = 'set_push_env'
+        dag_id = 'ingest_raw_data_dag'
+        env = context['ti'].xcom_pull(
+            task_ids=task_ids,
+            key='env',
+            dag_id = dag_id
+            )
+        logger.info(f"Pulled env from {dag_id}/{task_ids} XCom: {env}")
+        return env
 
+    @task
+    def price_technicals_lastday(env: str):
+        logger.info(f'running price_technicals_lastday for {env} environment')
+        bash_command=f"dbt run -s price_technicals_lastday  --profiles-dir {dbt_dir}/config --project-dir {dbt_dir} --target {env}"
+        logger.info(f'running command: {bash_command}')
+        result = subprocess.run(
+            bash_command,
+            shell=True,
+            check=True,
+            capture_output=True,
+            text=True
+        )
+        logger.info(result.stdout)
+    
     triggered_etf_transformations_dag = TriggerDagRunOperator(
         trigger_dag_id="etf_transformations_dag",
         task_id="triggered_etf_transformations_dag",
+        execution_date="{{ execution_date }}",
         wait_for_completion=False,
         deferrable=False
     )
-
-    price_technicals >> triggered_etf_transformations_dag
+    
+    env = pull_env()
+    price_technicals_lastday(env) >> triggered_etf_transformations_dag
 
 dag_instance = ticker_transformations_dag()
